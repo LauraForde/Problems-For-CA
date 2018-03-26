1194a7716e5fb8eae1b486bf16e39d50073ab028
LauraForde <G00315765@gmit.ie>
2018-03-26 16:18:50 +0100

    Attempting four and nine

---
+++ 04 - Sublsum.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(define (sublsum l)
+
+)
+
+(sublsum (list 1 2 3 4 -5))
+(sublsum (list 1 2 3 4 5))
---
+++ 09 - Sod2.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(define (sod2 x y z)
+    
+    (if (null? x)
+        '()
+        (cond
+            ( (= 0(modulo (+(car x) (car y) (car z)) 2)) (cons (0) (sod2 (cdr x) (cdr y) (cdr z))))
+            (else (cons (1) (sod2 (cdr x) (cdr y) (cdr z))))
+        )
+    )   
+    
+)
+
+(sod2 (list 0 0 0 0 1 1 1 1) (list 0 0 1 1 0 0 1 1) (list 0 1 0 1 0 1 0 1))
